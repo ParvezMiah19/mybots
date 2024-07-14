a677b3fb4f27939bfa6098fe4e1f4e806920239a
Parvez <ahmedparvez188@gmail.com>
2024-07-14 17:57:17 +0600

    Added joints and updated generate.py and simulate.py for three-link robot

--- generate.py
+++ generate.py
@@ -1,4 +1,18 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
-pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Ground", pos=[0, 0, 0], size=[10, 10, 0.5])  # Adding a ground plane for better visualization
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-1, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[1, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, 0.5])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 0.5])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -1,12 +1,12 @@
 import pybullet as p
+import pybullet_data
 import time
-
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-
-for i in range(1000):
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
+worldId = p.loadSDF("world.sdf")
+robotId = p.loadURDF("body.urdf")
+while p.isConnected():
     p.stepSimulation()
-    time.sleep(1/60)
-    print(i)
-
-p.disconnect()
+    time.sleep(1./240.)
